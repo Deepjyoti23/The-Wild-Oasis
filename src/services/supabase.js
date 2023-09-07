4222c8a39155a591b606cd7996adf32d745afc26
Deepjyoti23 <deepjyoti0401@gmail.com>
2023-09-07 16:25:31 +0530

    add supabse connection url

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://zhdlwpftokkcqdgkvcjb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoZGx3cGZ0b2trY3FkZ2t2Y2piIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwMzUyNjgsImV4cCI6MjAwOTYxMTI2OH0.ObRO5De8fm3I-G9PxNWo4iiWNtz_anMwfoh6YzgSoUM";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
